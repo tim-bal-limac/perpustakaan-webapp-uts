58ed3bc159ea3fe4462e31573bdc70f0bf82eda5
unknown <bayu28.bap@gmail.com>
2017-11-03 06:29:07 +0700

    Original

---
+++ src/main/java/limac/timbal/perpustakaanwebapputs/PerpustakaanWebappUtsApplication.java
@@ -0,0 +1,12 @@
+package limac.timbal.perpustakaanwebapputs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerpustakaanWebappUtsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerpustakaanWebappUtsApplication.class, args);
+	}
+}
---
+++ src/test/java/limac/timbal/perpustakaanwebapputs/PerpustakaanWebappUtsApplicationTests.java
@@ -0,0 +1,16 @@
+package limac.timbal.perpustakaanwebapputs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PerpustakaanWebappUtsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
