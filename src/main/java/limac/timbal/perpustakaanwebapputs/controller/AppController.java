380fe3b0401494330f8fe179097651e48efdb2c3
Aguz Riyanto <aguzri10@gmail.com>
2017-11-04 14:08:53 +0700

    Agus-Lutfi

--- src/main/java/limac/timbal/perpustakaanwebapputs/controller/AppController.java
+++ src/main/java/limac/timbal/perpustakaanwebapputs/controller/AppController.java
@@ -1,10 +1,15 @@
 package limac.timbal.perpustakaanwebapputs.controller;
 
+import limac.timbal.perpustakaanwebapputs.entitiy.Buku;
 import limac.timbal.perpustakaanwebapputs.repo.BukuRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AppController {
@@ -17,6 +22,38 @@ public class AppController {
     @RequestMapping("/daftar-buku")
     public void getDaftarBuku(Model model){
       model.addAttribute("daftarBuku",bukuRepo.findAll());
+    }
+    
+    @RequestMapping(value = "/tambah-data", method = RequestMethod.GET)
+    public void getTambahData(@ModelAttribute("buku")Buku buku, BindingResult binding){
+    }
+    
+    @RequestMapping(value = "/tambah-data", method = RequestMethod.POST)
+    public String saveTambahData(@ModelAttribute("buku")Buku buku, BindingResult binding) {
+        System.out.println(buku.getId());
+        bukuRepo.save(buku);
+        return "redirect:/daftar-buku";
+    }
+    
+    @RequestMapping(value = "/edit", method = RequestMethod.GET)
+    public void getEditData(@RequestParam(name = "id", required = false) String id,
+            @ModelAttribute("buku") Buku buku, BindingResult binding) {
+        Buku ebuku = bukuRepo.findById(id);
+        buku.setId(ebuku.getId());
+        buku.setJudul_buku(ebuku.getJudul_buku());
+        buku.setPenulis(ebuku.getPenulis());
+    }
+    
+    @RequestMapping(value = "/edit", method = RequestMethod.POST)
+    public String saveEditData(@ModelAttribute("buku") Buku buku, BindingResult binding) {
+    bukuRepo.save(buku);
+    return "redirect:/daftar-buku";
+    }
+    
+    @RequestMapping("/delete")
+    public String deleteData(@RequestParam(name = "id", required = true) String id) {
+        bukuRepo.delete(id);
+        return "redirect:/daftar-buku";
     }
 
 }
---
+++ uts_oop2.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 04 Nov 2017 pada 04.33
+-- Versi Server: 10.1.25-MariaDB
+-- PHP Version: 5.6.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `uts_oop2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `buku`
+--
+
+CREATE TABLE `buku` (
+  `id` int(3) NOT NULL,
+  `judul_buku` varchar(100) NOT NULL,
+  `penulis` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `buku`
+--
+
+INSERT INTO `buku` (`id`, `judul_buku`, `penulis`) VALUES
+(1, 'Koala Kumal', 'Raditya Dika'),
+(2, 'Marmut Merah Jambu', 'Raditya Dika');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `buku`
+--
+ALTER TABLE `buku`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `buku`
+--
+ALTER TABLE `buku`
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
